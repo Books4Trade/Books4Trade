52fd742838c625da1930f5d62917616919112523
adamchappell00 <adam.chappell00@gmail.com>
2022-03-08 13:57:02 -0600

    Notifications added constructors, relationships

--- src/main/java/com/example/books4trade/models/Notification.java
+++ src/main/java/com/example/books4trade/models/Notification.java
@@ -23,8 +23,45 @@ public class Notification {
     @JoinColumn(name = "user_id")
     private User toUser;
 
-    public Notifications
+    // Constructors
+    public Notification(){}
+    public Notification(long id, Boolean hasRead, String body, User fromUser, User toUser) {
+        this.id = id;
+        this.hasRead = hasRead;
+        this.body = body;
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+    }
 
-
-
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public Boolean getHasRead() {
+        return hasRead;
+    }
+    public void setHasRead(Boolean hasRead) {
+        this.hasRead = hasRead;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public User getFromUser() {
+        return fromUser;
+    }
+    public void setFromUser(User fromUser) {
+        this.fromUser = fromUser;
+    }
+    public User getToUser() {
+        return toUser;
+    }
+    public void setToUser(User toUser) {
+        this.toUser = toUser;
+    }
 }
