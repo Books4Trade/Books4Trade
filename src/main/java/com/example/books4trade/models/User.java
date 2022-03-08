6d1036175a866f711ffcb1025c270fc32dd599a1
adamchappell00 <adam.chappell00@gmail.com>
2022-03-08 10:23:51 -0600

    Constructors and Get/Set Methods added

--- src/main/java/com/example/books4trade/models/User.java
+++ src/main/java/com/example/books4trade/models/User.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name="users")
 public class User {
+    // FIELDS
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -26,5 +27,73 @@ public class User {
 
     @Column(nullable = false)
     private String location;
+    // RELATIONSHIPS
 
+
+    // CONTSTRUCTORS
+    public User(){}
+    public User(User copy){
+        id = copy.id;
+        username = copy.username;
+        password = copy.password;
+        email = copy.email;
+        firstName = copy.firstName;
+        lastName = copy.lastName;
+        location = copy.location;
+
+    }
+    public User(long id, String username, String password, String firstName, String lastName, String email, String location) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.location = location;
+    }
+
+
+    // User Field Getters & Setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
