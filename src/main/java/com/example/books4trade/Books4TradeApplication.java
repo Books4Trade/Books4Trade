42c2de8179b7e7cb0b8f35f6adbcdab2b9f4b373
adamchappell00 <adam.chappell00@gmail.com>
2022-03-08 09:32:34 -0600

    Startup

---
+++ src/main/java/com/example/books4trade/Books4TradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.books4trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Books4TradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Books4TradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/books4trade/Books4TradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.books4trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Books4TradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
