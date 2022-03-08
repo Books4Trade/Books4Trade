b527f2da1d1d547f112319de6902fbf75e3f4af4
adamchappell00 <adam.chappell00@gmail.com>
2022-03-08 10:19:25 -0600

    Initial columns set up, needs methods and relationships

--- src/main/java/com/example/books4trade/models/User.java
+++ src/main/java/com/example/books4trade/models/User.java
@@ -1,4 +1,30 @@
 package com.example.books4trade.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String location;
+
 }
