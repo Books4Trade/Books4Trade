b9fa86d97d504212f428c1a596422e51257b15b1
adamchappell00 <adam.chappell00@gmail.com>
2022-03-11 12:34:19 -0600

    refactored OwnedBooks Controller errors in declaration and returns

--- src/main/java/com/example/books4trade/controllers/OwnedBookController.java
+++ src/main/java/com/example/books4trade/controllers/OwnedBookController.java
@@ -4,6 +4,7 @@ import com.example.books4trade.models.OwnedBook;
 import com.example.books4trade.models.Type;
 import com.example.books4trade.models.User;
 import com.example.books4trade.repositories.BookRepository;
+import com.example.books4trade.repositories.OwnedBookRepository;
 import com.example.books4trade.repositories.TypeRepository;
 import com.example.books4trade.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,17 +21,19 @@ public class OwnedBookController {
         private final UserRepository usersDao;
         private final TypeRepository typesDao;
         private final BookRepository booksDao;
+        private final OwnedBookRepository ownedBooksDao;
 
-        public TypeController(UserRepository usersDao, TypeRepository typesDao, BookRepository booksDao) {
+        public OwnedBookController(UserRepository usersDao, TypeRepository typesDao, BookRepository booksDao, OwnedBookRepository ownedBooksDao) {
                 this.usersDao = usersDao;
                 this.typesDao = typesDao;
                 this.booksDao = booksDao;
+                this.ownedBooksDao = ownedBooksDao;
         }
 
         @GetMapping("/books/{id}/copy/add")
         public String showCreateOwnedBook(@PathVariable long id, Model model) {
                 model.addAttribute("book", booksDao.findById(id));
-                return"/";
+                return"/owned-book/create-copy";
         }
 
         @PostMapping("/books/{id}/copy/add")
@@ -43,6 +46,17 @@ public class OwnedBookController {
                 ownedBook.setType(bookType);
                 return "redirect:/owned-book";
         }
+        @GetMapping("/books/{id}/copy")
+        public String showAllOwnedCopies(@PathVariable long id, Model model){
+                model.addAttribute("allBooks",)
+                return "books/index";
+        }
+
+        @GetMapping("/books/{id}/copy/{copyid}")
+        public String showOwnedBook(@PathVariable long id, @PathVariable long copyid, Model model){
+                return "/owned-book/show";
+        }
+
 
 
 }
