f017f93bf82e3c7171090a2b811733f823ccfcc0
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-03-11 00:15:56 -0600

    still working on api

--- src/main/resources/static/js/books_api.js
+++ src/main/resources/static/js/books_api.js
@@ -4,10 +4,24 @@ $(document).ready(function(){
     const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key='+ GB_API;
 
         function handleResponse(response) {
+            fetch()
         for (var i = 0; i < response.items.length; i++) {
         var item = response.items[i];
         // in production code, item.text should have the HTML entities escaped.
-        document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
-    }
+        // document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
+            console.log(item.volumeInfo.title);
+        }
     }
 });
+
+//<script>
+//   function handleResponse(response) {
+//     for (var i = 0; i < response.items.length; i++) {
+//       var item = response.items[i];
+//       // in production code, item.text should have the HTML entities escaped.
+//       document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
+//       console.log(item);
+//     }
+//   }
+// </script>
+// <script src="https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse"></script>
