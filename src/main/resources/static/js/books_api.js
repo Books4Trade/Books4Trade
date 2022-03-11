bc893a105b9ea5757a10077d43a07c6b79244920
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-03-11 13:57:48 -0600

    api has been implemented; still needs some work but is funcitonal

--- src/main/resources/static/js/books_api.js
+++ src/main/resources/static/js/books_api.js
@@ -1,27 +1,48 @@
 "use strict";
 $(document).ready(function(){
     //  API URL
-    const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key='+ GB_API;
+    const GB_URL_TITLE = 'https://www.googleapis.com/books/v1/volumes?q=+intitle:';
+    const GB_URL_AUTHOR = 'https://www.googleapis.com/books/v1/volumes?q=+inauthor:';
 
-        function handleResponse(response) {
-            fetch()
-        for (var i = 0; i < response.items.length; i++) {
-        var item = response.items[i];
-        // in production code, item.text should have the HTML entities escaped.
-        // document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
-            console.log(item.volumeInfo.title);
-        }
-    }
+    //  FUNCTION TO SEARCH BY TITLE
+    $('#submit').click(function(e){
+        e.preventDefault();
+        let title = $('#books').val();
+
+        fetch(`${GB_URL_TITLE}${(title).toLowerCase()}&key=${GB_API}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                let title = data.title;
+                let content = $('#content');
+                for (var i = 0; i < data.items.length; i++) {
+                    var item = data.items[i];
+                    // in production code, item.text should have the HTML entities escaped.
+                    document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title + ", Author: " + item.volumeInfo.authors;
+                    console.log(item);
+                }
+            })
+            .catch(err => console.error(err));
+    })
+
+    //  FUNCTION TO SEARCH BY AUTHOR
+    $('#submitAuthor').click(function(e){
+        e.preventDefault();
+        let author = $('#booksAuthor').val();
+
+        fetch(`${GB_URL_AUTHOR}${(author).toLowerCase()}&key=${GB_API}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                let title = data.title;
+                let content = $('#content');
+                for (var i = 0; i < data.items.length; i++) {
+                    var item = data.items[i];
+                    // in production code, item.text should have the HTML entities escaped.
+                    document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title + ", Author: " + item.volumeInfo.authors;
+                    console.log(item);
+                }
+            })
+            .catch(err => console.error(err));
+    })
 });
-
-//<script>
-//   function handleResponse(response) {
-//     for (var i = 0; i < response.items.length; i++) {
-//       var item = response.items[i];
-//       // in production code, item.text should have the HTML entities escaped.
-//       document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
-//       console.log(item);
-//     }
-//   }
-// </script>
-// <script src="https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse"></script>
