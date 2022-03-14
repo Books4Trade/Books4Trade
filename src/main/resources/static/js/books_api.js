740935b536207d06b319d5b2f02525a10fd1dbf7
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-03-14 14:51:21 -0500

    search landing page is complete; will need to style further but works accordingly

--- src/main/resources/static/js/books_api.js
+++ src/main/resources/static/js/books_api.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
             .then(data => {
                 console.log(data);
                 let content = document.getElementById("search-results");
-                // content.innerHTML = "<form>"
                 for (let i = 0; i < data.items.length; i++) {
                     let item = data.items[i],
                     title = item.volumeInfo.title,
@@ -22,14 +21,19 @@ $(document).ready(function(){
                     preview = item.volumeInfo.previewLink,
                     img = item.volumeInfo.imageLinks.smallThumbnail;
                     content.innerHTML +=
-                        "<br>"
-                        + "<img src='"+ img + "' alt='book cover'/> "
-                        + title
-                        + ", Author: " + author
-                        + " <a href=\"" + preview + "\">Preview Book</a>"
-                        + "<br>";
-                    console.log(title);
+                        "<br><li class='d-flex align-items-center'>" +
+                        "<img src='" + img + "' alt='book cover'/> " +
+                        "<div class='d-inline-flex flex-column justify-content-center align-items-center'>" +
+                        "<h5>" + title + "</h5>" +
+                        "<p>Author: " + author + "</p>" +
+                        "<a href='" + preview + "'>Preview Book</a>" + " " +
+                        "<input class='form-check-input' type='radio' name='flexRadio'" +
+                        "</div>" +
+                        "</li><br>";
                 }
+                content.innerHTML +=
+                    "<br>" +
+                    "<div class='d-grid gap-2'><button class='btn btn-primary'>Submit</button></div></form>";
             })
             .catch(err => console.error(err));
     })
@@ -43,14 +47,27 @@ $(document).ready(function(){
             .then(resp => resp.json())
             .then(data => {
                 console.log(data);
-                let title = data.title;
-                let content = $('#content');
-                for (var i = 0; i < data.items.length; i++) {
-                    var item = data.items[i];
-                    // in production code, item.text should have the HTML entities escaped.
-                    document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title + ", Author: " + item.volumeInfo.authors;
-                    console.log(item);
+                let content = document.getElementById("search-results");
+                for (let i = 0; i < data.items.length; i++) {
+                    let item = data.items[i],
+                        title = item.volumeInfo.title,
+                        author = item.volumeInfo.authors,
+                        preview = item.volumeInfo.previewLink,
+                        img = item.volumeInfo.imageLinks.smallThumbnail;
+                    content.innerHTML +=
+                        "<br><li class='d-flex align-items-center'>" +
+                        "<img src='" + img + "' alt='book cover'/> " +
+                        "<div class='d-inline-flex flex-column justify-content-center align-items-center'>" +
+                        "<h5>" + title + "</h5>" +
+                        "<p>Author: " + author + "</p>" +
+                        "<a href='" + preview + "'>Preview Book</a>" + " " +
+                        "<input class='form-check-input' type='radio' name='flexRadio'" +
+                        "</div>" +
+                        "</li><br>";
                 }
+                content.innerHTML +=
+                    "<br>" +
+                    "<div class='d-grid gap-2'><button class='btn btn-primary'>Submit</button></div></form>"
             })
             .catch(err => console.error(err));
     })
