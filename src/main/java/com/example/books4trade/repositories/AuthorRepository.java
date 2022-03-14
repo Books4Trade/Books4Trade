a8b1cd7fb63cc41ae64e56b52484ad6bebeb67a0
adamchappell00 <adam.chappell00@gmail.com>
2022-03-14 18:15:08 -0500

    author and book list methods for search functions added to Author and BookRepository

--- src/main/java/com/example/books4trade/repositories/AuthorRepository.java
+++ src/main/java/com/example/books4trade/repositories/AuthorRepository.java
@@ -1,8 +1,16 @@
 package com.example.books4trade.repositories;
 
 import com.example.books4trade.models.Author;
+import com.example.books4trade.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     Author findAuthorByFullname(String fullname);
+
+    @Query("from Authors a where a.fullname like %:term%")
+    List<Author> searchByFullnameLike(@Param("term") String term);
 }
--- src/main/java/com/example/books4trade/repositories/BookRepository.java
+++ src/main/java/com/example/books4trade/repositories/BookRepository.java
@@ -2,6 +2,15 @@ package com.example.books4trade.repositories;
 
 import com.example.books4trade.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("from Books b where b.title like %:term%")
+    List<Book> searchByTitleLike(@Param("term") String term);
+
+    List<Book> searchByAuthorId(long id);
 }
