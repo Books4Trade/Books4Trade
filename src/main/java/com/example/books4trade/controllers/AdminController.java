ed1cf5476941d0d110ca92d938aaeed83bfb6541
adamchappell00 <adam.chappell00@gmail.com>
2022-03-21 13:34:28 -0500

    removed extra / on template paths

--- src/main/java/com/example/books4trade/controllers/AdminController.java
+++ src/main/java/com/example/books4trade/controllers/AdminController.java
@@ -39,7 +39,7 @@ public class AdminController {
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = usersDao.findByUsername(currentUser.getUsername());
         model.addAttribute("user", user);
-        return"/users/dashboard";
+        return"admin/dashboard";
     }
 
     @GetMapping("/test/email")
