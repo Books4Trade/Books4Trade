5c9b6aea35b670510d97b0c6cc175d4533f433dc
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-03-24 00:57:14 -0500

    added custom query

--- src/main/java/com/example/books4trade/repositories/TradeRepository.java
+++ src/main/java/com/example/books4trade/repositories/TradeRepository.java
@@ -1,8 +1,15 @@
 package com.example.books4trade.repositories;
 
 import com.example.books4trade.models.Trade;
+import com.example.books4trade.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
+    List<Trade> findAll();
 
+    @Query("from Trade t where t.item1.user = ?1")
+    List<Trade> findTradeByUser(User user);
 }
